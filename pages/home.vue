fe0427839fdf937609b47031986b7b9b5c4d413f
shuhei.tachikawa <shuhei.tusx@gmail.com>
2020-12-26 02:59:59 +0900

    add head

--- pages/home.vue
+++ pages/home.vue
@@ -21,7 +21,7 @@ export default {
     const { API } = withSSRContext(context);
     const { data } = await API.graphql(graphqlOperation(listTodos));
     console.log(data);
-    console.log(process.browser)
+    console.log(process.browser);
     return {
       todos: data.listTodos.items
     };
@@ -41,6 +41,34 @@ export default {
       };
       await API.graphql(graphqlOperation(createTodo, { input }));
     }
+  },
+  head() {
+    return {
+      title: this.todos[0].id,
+      meta: [
+        //{ name: 'description', content: this.text },
+
+        //{ property: 'og:type', content: 'website' },
+        { property: "og:title", content: this.todos[0].id },
+        { property: "og:description", content: this.todos[0].id },
+        //{ property: 'og:url', content: 'https://bubekiti.com' },
+        //{ property: 'og:site_name', content: 'Bubekiti Web creating' },
+        // { property: 'og:image', content: this.image },
+
+        //{ property: 'fb:app_id', content: '591933487923354' },
+
+        { name: "twitter:card", content: "summary_large_image" },
+        { name: "twitter:site", content: "@bubekiti" },
+        { name: "twitter:domain", content: "bubekiti.com" },
+        { name: "twitter:title", content: this.todos[0].id },
+        {
+          name: "twitter:description",
+          content:
+            "神奈川県秦野市を拠点に活動しているWeb制作フリーランス(SOHO)です。神奈川・東京を中心にWeb制作業務(ホームページ制作 / HTMLコーディング / レスポンシブデザイン / CMS構築)を承っております。"
+        }
+        // { property: 'twitter:image', content: this.image },
+      ]
+    };
   }
 };
 </script>
