3220c9f2f0708f6424d44bd2b80b50875e4f6066
shuhei.tachikawa <shuhei.tusx@gmail.com>
2020-12-25 22:07:39 +0900

    add ssr

--- plugins/amplify.js
+++ plugins/amplify.js
@@ -3,5 +3,6 @@ import Amplify from 'aws-amplify'
 import '@aws-amplify/ui-vue'
 import awsExports from '@/src/aws-exports'
 
-Amplify.configure(awsExports)
+// Amplify.configure(awsExports)
+Amplify.configure({ ...awsExports, ssr: true })
 Vue.use(Amplify)
