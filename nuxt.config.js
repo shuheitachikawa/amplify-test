31b49a88aa91650f22b243b8c15bcd871b58232e
shuhei.tachikawa <shuhei.tusx@gmail.com>
2020-12-25 22:13:12 +0900

    change to server

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [{ src: '~/plugins/amplify.js', mode: 'client' }],
+  plugins: [{ src: '~/plugins/amplify.js', mode: 'server' }],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
