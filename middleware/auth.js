6e2186d15981582ca6b2b0e3f187b1dff3ee3bba
shuhei.tachikawa <shuhei.tusx@gmail.com>
2021-01-17 09:05:37 +0900

    remove login check

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,36 +1,18 @@
-import { Auth } from "aws-amplify";
-import { withSSRContext } from "aws-amplify";
+// import { Auth } from "aws-amplify";
+// import { withSSRContext } from "aws-amplify";
 
-export default async function ({ route, redirect }) {
-  const user = await Auth.currentAuthenticatedUser()
-    .then((res) => {
-      console.log(res)
-      return res.username
-    })
-    .catch(() => {
-      return ''
-    })
-  if (!user) {
-    if (route.path.split('/')[1] !== 'login' || route.path.split('/')[1] !== 'tabelog') {
-      return redirect('/login')
-    }
-  }
-}
-
-// export default async function getServerSideProps(context, { route, redirect }) {
-//   const { Auth } = withSSRContext({ req: context.req });
+// export default async function ({ route, redirect }) {
 //   const user = await Auth.currentAuthenticatedUser()
-//     .then(res => {
-//       console.log(process.browser);
-//       console.log(res);
-//       return res.username;
+//     .then((res) => {
+//       console.log(res)
+//       return res.username
 //     })
 //     .catch(() => {
-//       return "";
-//     });
+//       return ''
+//     })
 //   if (!user) {
-//     if (route.path.split("/")[1] !== "login") {
-//       return redirect("/login");
+//     if (route.path.split('/')[1] !== 'login') {
+//       return redirect('/login')
 //     }
 //   }
 // }
